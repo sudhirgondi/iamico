8ee41782de00ebffd9973d51e3fa3342d4a8c0a3
Sudhir Gondi <sudhirgondi@gmail.com>
2013-09-20 13:35:53 -0700

    Go iAmico

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Iamico::Application.config.secret_key_base = '848ad59e16a2eb4fecd0a8e4f928b212f53445f0baa4da9f28c3a1070d20c89948cc026393369a0f0a7590aa1a4d5badfea8d20037fd29a45399fe35b9dd2e66'
